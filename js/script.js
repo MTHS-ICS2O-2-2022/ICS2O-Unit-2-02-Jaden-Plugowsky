6318dc8d5031493d43e9b2c2bc01059cd5a83686
Jaden Plugowsky <124389316+Jaden-Plugowsky@users.noreply.github.com>
2023-03-16 21:40:10 +0000

    initial website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Jaden Plugowsky All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jaden Plugowsky
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+function doMathClicked() {
+  document.getElementById("perimeter-math").innerHTML = "<p class='mdl-content'>2(5 + 3) = " + (2 * (5 + 3)) + "</p>"
+  document.getElementById("area-math").innerHTML = "<p class='mdl-content'>5 × 3 = " + (5 * 3) + "</p>"
+}
